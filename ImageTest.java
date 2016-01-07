73aed2ffbcc86be5bfe141e17c168031e88e313b
nicolecheng <ncheng@stuy.edu>
2016-01-07 17:49:39 -0500

    start of image processing code

---
+++ ImageTest.java
@@ -0,0 +1,5 @@
+// these import statements prompt the code for working with images
+import java.io.File; // used to create a File object
+import java.io.IOException; // used to catch IO Exception that could occur during the reading/writing of an image file
+import java.awt.image.BufferedImage; // created buffer to hold image
+import javax.imageio.ImageIO; // perform read and write operation
