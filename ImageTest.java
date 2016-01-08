9d9468877957b4d1781ed2a89ad46a02717280e6
nicolecheng <ncheng@stuy.edu>
2016-01-08 13:08:00 -0500

    displays a photo

--- ImageTest.java
+++ ImageTest.java
@@ -14,9 +14,8 @@ public class ImageTest{
     public static void main (String[]args) throws IOException{
 	int width = 1000; // of image
 	int length = 900;
-	BufferedImage buffer = null;
 	File f = null;
-
+	/*
 	try{
 	    f = new File("PrivateInvestigator.png");
 	    buffer = new BufferedImage(width, length, BufferedImage.TYPE_INT_ARGB);
@@ -25,7 +24,32 @@ public class ImageTest{
 	}catch(IOException e){
 	    print("error: did not successfully read file -- " + e);
 	}
+	*/
+	SwingUtilities.invokeLater(new Runnable(){
+		public void run(){
+		    BufferedImage buffer = null;
+		    JFrame j = new JFrame("jframe");
+		    j.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		    try{
+			buffer = ImageIO.read(new File("PrivateInvestigator.png"));
+		    }catch(Exception e){
+			e.printStackTrace();
+			print("error in rendering photo!!");
+			//System.exit(1); // exit bc of abnormal termination
+		    }
+		    ImageIcon imageIcon = new ImageIcon("PrivateInvestigator.png");
+		    JLabel jLabel = new JLabel();
+		    jLabel.setIcon(imageIcon);
+		    j.getContentPane().add(jLabel, BorderLayout.CENTER);
 
+		    j.pack();
+		    j.setLocationRelativeTo(null);
+		    j.setVisible(true);
+		    print("..");
+		}
+	    });
+	//	run();
+	print("huzzah, you have reached the end of the code!");
     }
 }
 
